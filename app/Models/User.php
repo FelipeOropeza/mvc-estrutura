cb4175f7be2eee7d145783b11c1b30d3789cfd9d
FelipeOropeza <felipe2006.co@gmail.com>
2026-02-28 22:33:53 -0300

    refactor(validation): replace MinLength/MaxLength with smart Min/Max rules

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Core\Database\Model;
 use Core\Attributes\Required;
 use Core\Attributes\Email;
+use Core\Attributes\IsFloat;
 use Core\Attributes\MinLength;
 
 class User extends Model
@@ -23,7 +24,11 @@ class User extends Model
 
     #[Required] 
     #[MinLength(8)]
-    public ?string $password = null;    
+    public ?string $password = null;
+
+    #[Required]
+    #[IsFloat(7,2)]
+    public ?float $saldo = null;
     
     public ?string $created_at = null;
     public ?string $updated_at = null;
