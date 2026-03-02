8381aa694bfb34116599b8452353fedc6be12868
FelipeOropeza <felipe2006.co@gmail.com>
2026-03-02 02:07:49 -0300

    feat: melhorando a rota

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Controllers\HomeController;
+use Core\Routing\Route;
 
 /** @var \Core\Routing\Router $router */
 
@@ -9,3 +10,5 @@ use App\Controllers\HomeController;
 // ==========================================
 
 // A Rota /home não está mais aqui, ela foi movida para atributos no HomeController!
+
+Route::get('/', [HomeController::class, 'index']);
